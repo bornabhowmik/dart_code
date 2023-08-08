6bf63c0a2fa3487186d396a3d496362d5a6bc8b6
bornabhowmik <bornabhowmik4@gmail.com>
2023-08-08 10:04:46 +0600

    add dart code file

--- bin/Assignment-1.dart
+++ bin/Assignment-1.dart
@@ -1,3 +1,16 @@
 void main(){
-  print("Hello");
+
+  var a=7;
+  var b=3;
+  var A=a+b;
+  var B=a-b;
+  var C=a*b;
+  var D=a/b;
+  var E=a%b;
+
+  print(A);
+  print(B);
+  print(C);
+  print(D);
+  print(E);
 }
