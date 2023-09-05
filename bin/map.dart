957a8c65596b0816c5a52b6f75f43163c9905e22
bornabhowmik <bornabhowmik4@gmail.com>
2023-09-05 22:33:48 +0600

    add map in dart code

--- bin/map.dart
+++ bin/map.dart
@@ -1,6 +1,6 @@
 void main(){
   Map student={'name':'tom','age':'23'};
   print(student);
-  student.addAll({'dept':'civil','email':'tom@xyz.com'});
+  student.addAll({'dept':'civil'});
   print(student);
 }
